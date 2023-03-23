aaa48c2ed950cf4bd8359aadf5ef519c65f1be77
Mehrdad Sami <MehrdadSaami@gmail.com>
2023-03-23 16:07:48 +0330

    Add rspec, factorybot, faker

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Dara
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators.test_framework = :rspec
   end
 end
---
+++ spec/factories/wallets.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :wallet do
+    name { "MyString" }
+    balance { 1.5 }
+  end
+end
